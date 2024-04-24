e218c7ba75825323f640b68d3c93983d8d8bfa46
Sparrow007 <ankit.steven007@gmail.com>
2024-04-25 01:04:42 +0530

    currently added the experiment as the main entery point animation

--- app/src/main/java/com/example/composelearning/MainActivity.kt
+++ app/src/main/java/com/example/composelearning/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composelearning.animation.AnimateOneChip
+import com.example.composelearning.animation.ShowSliderToExperiment
 import com.example.composelearning.ui.theme.ComposeLearningTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    AnimateOneChip()
+                    ShowSliderToExperiment()
                 }
             }
         }
