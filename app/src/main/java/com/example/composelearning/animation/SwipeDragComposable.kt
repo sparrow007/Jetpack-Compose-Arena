2e0289e897386dbd98fc39744bbc031a7dff034a
Sparrow007 <ankit.steven007@gmail.com>
2024-05-09 05:09:52 +0530

    added the swipe to dismiss animation for the first time

--- app/src/main/java/com/example/composelearning/animation/SwipeDragComposable.kt
+++ app/src/main/java/com/example/composelearning/animation/SwipeDragComposable.kt
@@ -1,10 +1,13 @@
 package com.example.composelearning.animation
 
 import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.calculateTargetValue
 import androidx.compose.animation.splineBasedDecay
 import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.horizontalDrag
 import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -22,12 +25,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.util.VelocityTracker
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
 
 @Composable
@@ -92,9 +98,49 @@ private fun Modifier.swipeToDissmis(
         coroutineScope {
             while(true) {
 
+                val pointerId = awaitPointerEventScope {awaitFirstDown().id }
+                offset.stop() // now user points down the animation we will stop the ongoing animation
+                //Prepare for the velocity track and fling
+                val velocityTracker = VelocityTracker()
+                awaitPointerEventScope {
+                    horizontalDrag(pointerId) {
+                        val dragOffset = offset.value + it.position.x
+                        launch {
+                            offset.snapTo(dragOffset)
+                        }
+                        velocityTracker.addPosition(it.uptimeMillis, it.position)
+                        it.consume()
+                        //Three things we did in this block
+                        /***
+                         * 1. calculate the drag of horizontal of the user
+                         * 2. take the ui to user finger position and snapTo
+                         * 3. calculate the velocity of user's drag
+                         */
+                    }
+                }
+
+                val velocity  = velocityTracker.calculateVelocity().x
+                val targetOffset = decay.calculateTargetValue(offset.value, velocity)
+
+                //Now we checks or add the boundaries for the animation
+                offset.updateBounds(
+                    -size.width.toFloat(),
+                    size.width.toFloat()
+                )
+
+                if (targetOffset <= size.width.toFloat()) {
+                    offset.animateTo(targetValue = 0f, initialVelocity = velocity)
+
+                } else {
+                    //Now it can go out of boundries and we
+                    offset.animateDecay(velocity, decay)
+                    onDissmiss()
+                }
             }
         }
 
+    }.offset {
+        IntOffset(offset.value.toInt(), 0)
     }
 
 }
