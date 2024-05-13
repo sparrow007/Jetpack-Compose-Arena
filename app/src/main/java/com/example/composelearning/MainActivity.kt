a86abdf85439795dd28c65ea803ecd4bd5c0bc1a
Sparrow007 <ankit.steven007@gmail.com>
2024-05-13 16:50:39 +0530

    Update the status bar for the compose ui to show

--- app/src/main/java/com/example/composelearning/MainActivity.kt
+++ app/src/main/java/com/example/composelearning/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.composelearning
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import com.example.composelearning.animation.AnimateOneChip
 import com.example.composelearning.animation.ShowCardInStack
 import com.example.composelearning.animation.ShowSliderToExperiment
@@ -31,6 +36,9 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        actionBar?.hide()
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
     }
 }
 
