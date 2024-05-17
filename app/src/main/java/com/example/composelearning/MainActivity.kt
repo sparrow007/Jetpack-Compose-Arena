55eda4b13575d2316d28da0eab9bf58d74945530
Sparrow007 <ankit.steven007@gmail.com>
2024-05-18 01:41:50 +0530

    update the main activity for the carousel layout

--- app/src/main/java/com/example/composelearning/MainActivity.kt
+++ app/src/main/java/com/example/composelearning/MainActivity.kt
@@ -7,15 +7,23 @@ import android.view.WindowInsetsController
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import com.example.composelearning.animation.AnimateOneChip
+import com.example.composelearning.animation.CircularCarousel
 import com.example.composelearning.animation.ShowCardInStack
 import com.example.composelearning.animation.ShowSliderToExperiment
 import com.example.composelearning.animation.SwipeableCards
@@ -29,12 +37,41 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
+                val colors = listOf(
+                    Color.Blue,
+                    Color.Red,
+                    Color.Green,
+                    Color.Magenta,
+                    Color.Cyan,
+                )
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.surface,
-                ) {
-                    ShowCarouselLayoutView()
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        CircularCarousel(
+                            numItems = 24,
+                            itemFraction = .2f,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(200.dp)
+                        ) { index ->
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .background(
+                                        color = colors[index % colors.size]
+                                    ),
+                                contentAlignment = Alignment.Center,
+                            ) {
+                                Text(
+                                    text = index.toString(),
+                                    color = Color.White,
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
