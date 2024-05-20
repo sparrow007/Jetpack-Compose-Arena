33779b3aa6ed9c83b48d796236fb630ff013f0c8
Sparrow007 <ankit.steven007@gmail.com>
2024-05-20 22:25:13 +0530

    added the basic layout with child measurement and provide the layout max width and height

--- app/src/main/java/com/example/composelearning/animation/carousel/CarouselRotatedLayout.kt
+++ app/src/main/java/com/example/composelearning/animation/carousel/CarouselRotatedLayout.kt
@@ -1,2 +1,102 @@
 package com.example.composelearning.animation.carousel
 
+import android.annotation.SuppressLint
+import android.util.Log
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.FloatSpringSpec
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.SpringSpec
+import androidx.compose.animation.core.calculateTargetValue
+import androidx.compose.animation.splineBasedDecay
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.drag
+import androidx.compose.foundation.gestures.horizontalDrag
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.consumePositionChange
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
+import androidx.compose.ui.input.pointer.util.VelocityTracker
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import com.example.composelearning.animation.book.width
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlin.math.PI
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.roundToInt
+import kotlin.math.sin
+
+@Composable
+fun CarouselLayout(
+    numOfItems: Int,
+    modifier: Modifier = Modifier,
+    itemFraction: Float = 0.25f,
+    contentFactory: @Composable (Int)-> Unit,
+) {
+    require(numOfItems > 0) {"The number of items must be greater than 0"}
+    require(itemFraction > 0f && itemFraction < 1f) {"The item fraction must be between 0 and 1"}
+
+   Layout(modifier = modifier, content = {
+       repeat(numOfItems) {
+           Box(modifier = Modifier.fillMaxSize()) {
+               contentFactory(it)
+           }
+       }
+   }) { measurables, constraints ->
+       val itemDimension = constraints.maxHeight * itemFraction
+
+       val itemConstraints = Constraints.fixed(
+           width = itemDimension.toInt(),
+           height =  itemDimension.toInt()
+       )
+
+       val placeables = measurables.map { measurable -> measurable.measure(itemConstraints) }
+
+       layout(constraints.maxWidth, constraints.maxHeight) {
+
+       }
+   }
+
+}
