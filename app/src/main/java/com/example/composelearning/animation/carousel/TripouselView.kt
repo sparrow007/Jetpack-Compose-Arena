3309b22df8b8c4505dccac5f34f54c56819d356e
Sparrow007 <ankit.steven007@gmail.com>
2024-05-23 04:14:04 +0530

    started the magazine project

---
+++ app/src/main/java/com/example/composelearning/animation/carousel/TripouselView.kt
@@ -0,0 +1,219 @@
+package com.example.composelearning.animation.carousel
+
+import android.annotation.SuppressLint
+import android.util.Log
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.FloatSpringSpec
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.SpringSpec
+import androidx.compose.animation.core.calculateTargetValue
+import androidx.compose.animation.splineBasedDecay
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.drag
+import androidx.compose.foundation.gestures.horizontalDrag
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.consumePositionChange
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
+import androidx.compose.ui.input.pointer.util.VelocityTracker
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import com.example.composelearning.animation.CircularCarousel
+import com.example.composelearning.animation.book.width
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlin.math.PI
+import kotlin.math.absoluteValue
+import kotlin.math.atan2
+import kotlin.math.cos
+import kotlin.math.roundToInt
+import kotlin.math.sin
+
+@Composable
+fun TripouselView(
+    modifier: Modifier = Modifier,
+) {
+    val listOfCard = remember {
+        mutableStateOf(
+            arrayListOf(
+                Color(0xff90caf9),
+                Color(0xfffafafa),
+                Color(0xffef9a9a),
+            ).reversed()
+        )
+    }
+    var properties by remember {
+        mutableStateOf(Triple<Float, Float, Float>(0f, 0f, 0f))
+    }
+
+    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.TopCenter) {
+
+        listOfCard.value.forEachIndexed { index, color ->
+
+            ItemView(
+                color = color,
+                modifier = Modifier
+                    .offset {
+                        getItemOffset(index)
+                    }
+                    .graphicsLayer {
+//                            rotationZ = getItemProperties(listOfCard.value.indexOf(color)).second
+//                            scaleX = getItemProperties(listOfCard.value.indexOf(color)).first
+//                            scaleY = getItemProperties(listOfCard.value.indexOf(color)).first
+                    }
+                    .swipeToDissmiss {
+                        listOfCard.value = listOf(color) + (listOfCard.value - color)
+                    }
+            )
+
+        }
+
+    }
+
+}
+
+private fun getItemProperties(index: Int): Triple<Float, Float, Float> {
+    return when(index) {
+        0 -> {
+            Triple(0.8f, -22f, 1f)
+        }
+        1 -> {
+            Triple(0.8f, 22f, 2f)
+        }
+        else -> {
+            Triple(1f, 0f, 3f)
+        }
+    }
+
+}
+
+private fun getItemOffset(index: Int): IntOffset {
+    return when(index) {
+        0 -> {
+            IntOffset(-170, 35)
+        }
+        1 -> {
+            IntOffset(170, 35)
+        }
+        else -> {
+            IntOffset(0, 0)
+        }
+    }
+}
+
+@Composable
+private fun ItemView(modifier: Modifier, color: Color) {
+    Card (
+        modifier = modifier.size(width = 210.dp, height = 280.dp),
+        shape = RoundedCornerShape(8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = color
+        ),
+        elevation = CardDefaults.elevatedCardElevation(
+            defaultElevation = 8.dp
+        )
+    ) {
+
+    }
+}
+
+@Composable
+private fun Modifier.swipeToDissmiss(
+    onDismiss: ()->Unit
+): Modifier = composed {
+    val offsetAnim = remember {
+        Animatable(0f)
+    }
+    pointerInput(Unit) {
+        val decay = splineBasedDecay<Float>(this)
+        coroutineScope {
+            while (true) {
+                val velocityTracker = VelocityTracker()
+                awaitPointerEventScope {
+                    horizontalDrag(awaitFirstDown().id) { change ->
+                        val horizontalOffset = offsetAnim.value + change.positionChange().x
+                        launch {
+                            offsetAnim.snapTo(horizontalOffset)
+                        }
+                        velocityTracker.addPosition(change.uptimeMillis, change.position)
+
+                    }
+                    val velocity = velocityTracker.calculateVelocity().x
+                    val targetOffsetX = decay.calculateTargetValue(offsetAnim.value, velocity)
+                    launch {
+                        if (targetOffsetX.absoluteValue <= size.width) {
+                            offsetAnim.animateTo(targetValue = 0f)
+                        } else {
+                            offsetAnim.animateDecay(velocity, decay) {
+                                onDismiss.invoke()
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+    }.offset {
+        IntOffset(offsetAnim.value.roundToInt(), 0)
+    }
+}
+
+@Preview (showSystemUi = true)
+@Composable
+private fun ShowTripouselView() {
+    MaterialTheme {
+        Surface {
+           Box(modifier = Modifier.fillMaxSize()) {
+               Column {
+                   Spacer(modifier = Modifier.height(40.dp))
+                   TripouselView()
+               }
+           }
+        }
+    }
+}
