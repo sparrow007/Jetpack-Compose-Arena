63ce7edf5086dc3405ff37a5e7d3120919c5e57e
sparrow007 <ankit.steven007@gmail.com>
2024-07-02 06:14:53 +0530

    spring animation is cool for now

--- app/src/main/java/com/example/composelearning/maths/path/EffectivePathDrawing.kt
+++ app/src/main/java/com/example/composelearning/maths/path/EffectivePathDrawing.kt
@@ -1,38 +1,60 @@
 package com.example.composelearning.maths.path
 
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 
-@Preview
+@Preview(showSystemUi = true)
 @Composable
 fun CreateNormalPath() {
 
-    val infiniteTransition = rememberInfiniteTransition(label = "infinite transition")
-    val scale by infiniteTransition.animateFloat(
-        initialValue = 150f,
-        targetValue = 80f,
-        animationSpec = infiniteRepeatable(tween(1000), RepeatMode.Reverse),
-        label = "scale"
-    )
+    var animState by remember {
+        mutableStateOf(150f)
+    }
 
-    Canvas(modifier = Modifier.fillMaxSize()) {
-        val path = Path()
-        path.moveTo(0f, 150f)
-        //path.lineTo(0f, 150f)
-        path.quadraticBezierTo(size.width / 2, scale.toFloat(), size.width, 150f)
-        this.drawPath(path, Color.Blue, style = Stroke(5f))
+    val infiniteTransition = rememberInfiniteTransition(label = "infinite transition")
+    val scale = animateFloatAsState(
+        targetValue =
+        if (animState == 150f) 80f else 150f,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioHighBouncy,
+            stiffness = Spring.StiffnessLow
+        ), label = ""
+    )
+    Column(modifier = Modifier.fillMaxSize()) {
+        Canvas(modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()) {
+            val path = Path()
+            path.moveTo(0f, 150f)
+            //path.lineTo(0f, 150f)
+            path.quadraticBezierTo(size.width / 2, scale.value, size.width, 150f)
+            this.drawPath(path, Color.Blue, style = Stroke(5f))
+        }
+
+        Button(onClick = {
+            animState = if (animState == 150f) 80f else 150f
+        }) {
+            Text(text = "Animate")
+        }
 
     }
 
