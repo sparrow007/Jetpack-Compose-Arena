a10a46d0f465e9f5ea48994d8a23b776b840731c
sparrow007 <ankit.steven007@gmail.com>
2024-07-02 05:53:12 +0530

    added the infinite repeatable animation

--- app/src/main/java/com/example/composelearning/maths/path/EffectivePathDrawing.kt
+++ app/src/main/java/com/example/composelearning/maths/path/EffectivePathDrawing.kt
@@ -1,8 +1,14 @@
 package com.example.composelearning.maths.path
 
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
@@ -13,11 +19,19 @@ import androidx.compose.ui.tooling.preview.Preview
 @Composable
 fun CreateNormalPath() {
 
+    val infiniteTransition = rememberInfiniteTransition(label = "infinite transition")
+    val scale by infiniteTransition.animateFloat(
+        initialValue = 150f,
+        targetValue = 80f,
+        animationSpec = infiniteRepeatable(tween(1000), RepeatMode.Reverse),
+        label = "scale"
+    )
+
     Canvas(modifier = Modifier.fillMaxSize()) {
         val path = Path()
         path.moveTo(0f, 150f)
-        path.lineTo(0f, 150f)
-        path.quadraticBezierTo(size.width / 2, 150f, size.width, 150f)
+        //path.lineTo(0f, 150f)
+        path.quadraticBezierTo(size.width / 2, scale.toFloat(), size.width, 150f)
         this.drawPath(path, Color.Blue, style = Stroke(5f))
 
     }
