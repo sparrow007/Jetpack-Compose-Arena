2ad0d61c6f5e13033b7ed926ad0a21908097cbda
sparrow007 <ankit.steven007@gmail.com>
2024-07-17 02:36:12 +0530

    expriments with the bezier curves and handling control points

--- app/src/main/java/com/example/composelearning/maths/path/EffectivePathDrawing.kt
+++ app/src/main/java/com/example/composelearning/maths/path/EffectivePathDrawing.kt
@@ -1,13 +1,10 @@
 package com.example.composelearning.maths.path
 
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.spring
+import android.graphics.PointF
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,64 +14,83 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview(showSystemUi = true)
 @Composable
 fun CreateNormalPath() {
 
-    var animState by remember {
-        mutableStateOf(150f)
+//    var animState by remember {
+//        mutableStateOf(150f)
+//    }
+
+    var controlPoint by remember {
+        mutableStateOf(PointF(0f, 0f))
     }
-    val scale = animateFloatAsState(
-        targetValue =
-        if (animState == 150f) 80f else 150f,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioHighBouncy,
-            stiffness = Spring.StiffnessLow
-        ), label = ""
-    )
+
+    var controlPoint2 by remember {
+        mutableStateOf(PointF(0f, 0f))
+    }
+
+//    val scale = animateFloatAsState(
+//        targetValue =
+//        if (animState == 150f) 80f else 150f,
+//        animationSpec = spring(
+//            dampingRatio = Spring.DampingRatioHighBouncy,
+//            stiffness = Spring.StiffnessLow
+//        ), label = ""
+//    )
     Column(modifier = Modifier.fillMaxSize()) {
         Canvas(modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight()) {
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                detectDragGestures { change, dragAmount ->
+                    controlPoint = PointF(
+                        controlPoint.x + dragAmount.x,
+                        controlPoint.y + dragAmount.y
+                    )
+                }
+            }
+        ) {
+
             val path = Path()
-//            path.moveTo(0f, 150f)
-//            //path.lineTo(0f, 150f)
-//            path.quadraticBezierTo(size.width / 2, scale.value, size.width, 150f)
-//            this.drawPath(path, Color.Blue, style = Stroke(5f))
-
-
-            //Stretching path with quadraticBezierTo
             path.reset()
-            path.moveTo(0f, 200f)
-            path.lineTo(0f, 200f)
-            path.quadraticBezierTo(size.width / 2, 150f, size.width, 200f)
-            drawPath(path, Color.Blue, style = Stroke(5f))
+            if (controlPoint.x == 0f && controlPoint.y == 0f) {
+                controlPoint = PointF(size.width / 2f - 250f, size.height / 2f - 150f)
+            }
+            if (controlPoint2.x == 0f && controlPoint2.y == 0f) {
+                controlPoint2 = PointF(size.width / 2f + 250f, size.height / 2f - 250f)
+            }
 
-            path.reset()
-            path.moveTo(0f, 400f)
-            path.lineTo(0f, 400f)
-            path.quadraticBezierTo(size.width / 2, 350f, size.width, 400f)
-            drawPath(path, Color.Red, style = Stroke(5f))
+            val cp = PointF(size.width / 2f, size.height / 2f - 150f)
+            //controlPoint = cp
+            val point2 = PointF(size.width / 2f + 150f, size.height / 2f)
+            path.moveTo(size.width / 2f - 150f, size.height / 2f)
+
+            path.cubicTo(
+                controlPoint.x, controlPoint.y,
+                controlPoint2.x, controlPoint2.y,
+                point2.x, point2.y
+            )
 
 
-            //draw an gooey effect using path
-            path.reset()
-            path.moveTo(size.width / 2f, 370f)
-
-
-            path.quadraticBezierTo(size.width / 2, 550f, size.width, 600f)
-            drawPath(path, Color.Green, style = Stroke(5f))
+            drawPath(
+                path = path,
+                color = Color.Black,
+                style = Stroke(width = 5f)
+            )
 
         }
 
-//        Button(onClick = {
-//            animState = if (animState == 150f) 80f else 150f
-//        }) {
-//            Text(text = "Animate")
-//        }
+    }
 
+    fun Modifier.swipeToHandle() {
+        pointerInput(Unit) {
+            this.detectDragGestures { change, dragAmount ->
+
+            }
+        }
     }
 
 }
