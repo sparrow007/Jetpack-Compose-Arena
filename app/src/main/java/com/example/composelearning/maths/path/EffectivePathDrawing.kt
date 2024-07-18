2a07056128181c016d588e71d7c2242c08c4eb85
sparrow007 <ankit.steven007@gmail.com>
2024-07-18 19:12:59 +0530

    remove the comments

--- app/src/main/java/com/example/composelearning/maths/path/EffectivePathDrawing.kt
+++ app/src/main/java/com/example/composelearning/maths/path/EffectivePathDrawing.kt
@@ -1,6 +1,7 @@
 package com.example.composelearning.maths.path
 
 import android.graphics.PointF
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,12 +11,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview(showSystemUi = true)
 @Composable
 fun CreateNormalPath() {
+
 
 //    var animState by remember {
 //        mutableStateOf(150f)
@@ -30,46 +35,46 @@ fun CreateNormalPath() {
     }
 
     Column(modifier = Modifier.fillMaxSize()) {
-//        Canvas(modifier = Modifier
-//            .fillMaxSize()
-//            .pointerInput(Unit) {
-//                detectDragGestures { change, dragAmount ->
-//                    controlPoint = PointF(
-//                        controlPoint.x + dragAmount.x,
-//                        controlPoint.y + dragAmount.y
-//                    )
-//                }
-//            }
-//        ) {
-//
-//            val path = Path()
-//            path.reset()
-//            if (controlPoint.x == 0f && controlPoint.y == 0f) {
-//                controlPoint = PointF(size.width / 2f - 250f, size.height / 2f - 150f)
-//            }
-//            if (controlPoint2.x == 0f && controlPoint2.y == 0f) {
-//                controlPoint2 = PointF(size.width / 2f + 250f, size.height / 2f - 250f)
-//            }
-//
-//            val cp = PointF(size.width / 2f, size.height / 2f - 150f)
-//            //controlPoint = zcp
-//            val point2 = PointF(size.width / 2f + 150f, size.height / 2f)
-//            path.moveTo(size.width / 2f - 150f, size.height / 2f)
-//
-//            path.cubicTo(
-//                controlPoint.x, controlPoint.y,
-//                controlPoint2.x, controlPoint2.y,
-//                point2.x, point2.y
-//            )
-//
-//
-//            drawPath(
-//                path = path,
-//                color = Color.Black,
-//                style = Stroke(width = 5f)
-//            )
-//
-//        }
+        Canvas(modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                detectDragGestures { change, dragAmount ->
+                    controlPoint = PointF(
+                        controlPoint.x + dragAmount.x,
+                        controlPoint.y + dragAmount.y
+                    )
+                }
+            }
+        ) {
+
+            val path = Path()
+            path.reset()
+            if (controlPoint.x == 0f && controlPoint.y == 0f) {
+                controlPoint = PointF(size.width / 2f - 250f, size.height / 2f - 150f)
+            }
+            if (controlPoint2.x == 0f && controlPoint2.y == 0f) {
+                controlPoint2 = PointF(size.width / 2f + 250f, size.height / 2f - 250f)
+            }
+
+            val cp = PointF(size.width / 2f, size.height / 2f - 150f)
+            //controlPoint = zcp
+            val point2 = PointF(size.width / 2f + 150f, size.height / 2f)
+            path.moveTo(size.width / 2f - 150f, size.height / 2f)
+
+            path.cubicTo(
+                controlPoint.x, controlPoint.y,
+                controlPoint2.x, controlPoint2.y,
+                point2.x, point2.y
+            )
+
+
+            drawPath(
+                path = path,
+                color = Color.Black,
+                style = Stroke(width = 5f)
+            )
+
+        }
 
     }
 
