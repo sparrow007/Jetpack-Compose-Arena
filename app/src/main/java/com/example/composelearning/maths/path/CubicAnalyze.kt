c5d889326933c0811b4093b49a534d4165212480
sparrow007 <ankit.steven007@gmail.com>
2024-07-24 17:32:31 +0530

    we can use nativeCanvas in compose canvas

--- app/src/main/java/com/example/composelearning/maths/path/CubicAnalyze.kt
+++ app/src/main/java/com/example/composelearning/maths/path/CubicAnalyze.kt
@@ -1,13 +1,25 @@
 package com.example.composelearning.maths.path
 
+import android.graphics.Paint
+import android.graphics.Picture
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
+@Preview
 fun CubicAnalyze() {
     Canvas(modifier = Modifier.fillMaxSize()) {
-
+        val picture = Picture()
+        val paint = Paint()
+        val canvas = picture.beginRecording(size.width.toInt(), size.height.toInt())
+        canvas.drawCircle(100f, 100f, 50f, android.graphics.Paint())
+        this.drawIntoCanvas {
+            it.nativeCanvas.drawPicture(picture)
+        }
     }
 }
