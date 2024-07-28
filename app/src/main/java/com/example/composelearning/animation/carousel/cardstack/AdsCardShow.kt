41279dbcd95cfc12440427b262a32ed647d4afcb
sparrow007 <ankit.steven007@gmail.com>
2024-07-28 06:19:46 +0530

    Update the code for preview and gif

--- app/src/main/java/com/example/composelearning/animation/carousel/cardstack/AdsCardShow.kt
+++ app/src/main/java/com/example/composelearning/animation/carousel/cardstack/AdsCardShow.kt
@@ -2,27 +2,26 @@ package com.example.composelearning.animation.carousel.cardstack
 
 import android.annotation.SuppressLint
 import android.util.Log
-import android.view.MotionEvent
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
@@ -30,7 +29,6 @@ import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
@@ -94,12 +92,49 @@ fun AdsCardShow() {
                     modifier = Modifier
                         .width(120.dp)
                         .height(170.dp)
-                        .border(1.dp, Color.Gray, RoundedCornerShape(12.dp))
+                        .background(Color.White)
+                        .border(1.dp, Color.Gray, RoundedCornerShape(12.dp)),
+                    contentAlignment = Alignment.Center
                 ) {
+                    Column {
+
+                        BoxWithColor()
+                        BoxWithColor()
+                        BoxWithColor()
+
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        BoxWithColor()
+                        BoxWithColor()
+                        BoxWithColor()
+
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        BoxWithColor()
+                        BoxWithColor()
+                        BoxWithColor()
+
+
+                    }
+
                 }
             }
         }
     }
+}
+
+@Composable
+fun BoxWithColor() {
+    Box(
+        modifier = Modifier
+            .padding(
+                start = 10.dp,
+                end = 10.dp, top = 5.dp
+            )
+            .fillMaxWidth()
+            .height(5.dp)
+            .background(getRandomColor())
+    )
 }
 
 @SuppressLint("UnnecessaryComposedModifier", "ReturnFromAwaitPointerEventScope")
@@ -134,110 +169,25 @@ fun Modifier.onLayoutTouch(isPressed: (Boolean) -> Unit) = composed {
     }
 }
 
-
-@SuppressLint("ModifierFactoryUnreferencedReceiver")
-@OptIn(ExperimentalComposeUiApi::class)
-fun Modifier.cardLongClick(position: Int, totalSize: Int) = composed {
-    val xPosition = remember {
-        (totalSize - (position + 1)) * 30
-    }
-    val yPosition = remember {
-        (totalSize - (position + 1)) * 20
-    }
-
-    var rotationAnimation = remember {
-        Animatable(0f)
-    }
-    var longPressActive by remember { mutableStateOf(false) }
-
-
-    val scope = rememberCoroutineScope()
-    pointerInput(Unit) {
-        detectTapGestures(
-            onPress = {
-                tryAwaitRelease()
-                scope.launch {
-                    val rotation = ((totalSize - (position + 1)) * -10).toFloat()
-                    rotationAnimation.animateTo(
-                        rotation, spring(
-                            dampingRatio = Spring.DampingRatioNoBouncy,
-                            stiffness = Spring.StiffnessMedium
-                        )
-                    )
-                }
-                longPressActive = false
-            },
-            onLongPress = {
-                scope.launch {
-                    val rotation = ((totalSize - (position + 1)) * 10).toFloat()
-                    rotationAnimation.animateTo(
-                        rotation, spring(
-                            dampingRatio = Spring.DampingRatioNoBouncy,
-                            stiffness = Spring.StiffnessMedium
-                        )
-                    )
-                }
-                longPressActive = true
-            }
-        )
-    }
-        .pointerInteropFilter {
-            when (it.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    // User has pressed the button
-                    scope.launch {
-                        val rotation = ((totalSize - (position + 1)) * 10).toFloat()
-                        rotationAnimation.animateTo(
-                            rotation, spring(
-                                dampingRatio = Spring.DampingRatioNoBouncy,
-                                stiffness = Spring.StiffnessMedium
-                            )
-                        )
-                    }
-
-                }
-
-                MotionEvent.ACTION_UP -> {
-                    // User is no longer pressing the button
-                    scope.launch {
-                        val rotation = ((totalSize - (position + 1)) * -10).toFloat()
-                        rotationAnimation.animateTo(
-                            rotation, spring(
-                                dampingRatio = Spring.DampingRatioNoBouncy,
-                                stiffness = Spring.StiffnessMedium
-                            )
-                        )
-                    }
-                }
-
-                else -> false
-            }
-            true
-        }
-        .offset {
-            IntOffset(
-                xPosition,
-                -yPosition
-            )
-        }
-        .graphicsLayer {
-            cameraDistance = 12 * density
-            transformOrigin = TransformOrigin(1f, 1f)
-            rotationZ = rotationAnimation.value
-        }
+@Composable
+fun getRandomColor(): Color {
+    val colors = listOf(
+        Color(0xFFE57373),
+        Color(0xFF81C784),
+        Color(0xFF64B5F6),
+        Color(0xFFFFD54F),
+        Color(0xFF9575CD),
+        Color(0xFF4DB6AC),
+        Color(0xFFA1887F),
+        Color(0xFF90A4AE),
+        Color(0xFFE57373),
+        Color(0xFF81C784),
+        Color(0xFF64B5F6),
+        Color(0xFFFFD54F),
+        Color(0xFF9575CD),
+        Color(0xFF4DB6AC),
+        Color(0xFFA1887F),
+        Color(0xFF90A4AE),
+    )
+    return colors.random()
 }
-
-
-//@OptIn(ExperimentalMaterial3Api::class)
-//@Composable
-//fun CardsView(modifier: Modifier = Modifier) {
-//    Card(
-//        modifier = modifier
-//            .width(120.dp)
-//            .height(170.dp)
-//            .border(1.dp, Color.Gray, RoundedCornerShape(12.dp)),
-//        onClick = { /*TODO*/ }) {
-//
-//
-//    }
-//}
