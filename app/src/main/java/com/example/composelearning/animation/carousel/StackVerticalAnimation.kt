5245488100b811e4f2224a42dab4f1826995a47a
sparrow007 <ankit.steven007@gmail.com>
2024-09-08 12:36:48 +0530

    added the cards in the stack

--- app/src/main/java/com/example/composelearning/animation/carousel/StackVerticalAnimation.kt
+++ app/src/main/java/com/example/composelearning/animation/carousel/StackVerticalAnimation.kt
@@ -2,30 +2,58 @@ package com.example.composelearning.animation.carousel
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 @Composable
-fun StackCard(cards: List<Color>) {
+fun StackCard(colorList: List<Color>, size: Int) {
 
-    Box(modifier = Modifier.fillMaxSize()) {
-        cards.forEachIndexed { index, color ->
-
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        colorList.forEachIndexed { index, color ->
+            val density = LocalDensity.current
+            val yOffset = with(density) {
+                (size - (size - index) * 30).toDp()
+            }
+            //you can use linear interpolation to create scalable value range for 0 to 1
+            val scale = 1f - (size - index) * 0.02f
+            ShowCards(modifier = Modifier
+                .size(180.dp, 230.dp)
+                .offset(y = yOffset)
+                .graphicsLayer {
+                    scaleX = scale
+                    scaleY = scale
+                }, color = color)
         }
     }
 }
 
 @Composable
+fun ShowCards(modifier: Modifier, color: Color) {
+    Card(
+        colors = CardDefaults.cardColors(containerColor = color),
+        modifier = modifier
+    ) {}
+}
+
+@Composable
+@Preview(showSystemUi = true)
 fun ShowVerticalCardStack() {
     val list = listOf<Color>(
-        Color(0xFFE57373),
-        Color(0xFF81C784),
         Color(0xFF64B5F6),
         Color(0xFFFFD54F),
         Color(0xFF9575CD),
         Color(0xFF4DB6AC)
     )
-
+    StackCard(list, list.size)
 }
