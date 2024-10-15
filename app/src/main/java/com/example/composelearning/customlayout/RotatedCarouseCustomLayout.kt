a01129734d95a8cf7bfc2f86452cfdd0d9eaf2d6
sparrow007 <ankit.steven007@gmail.com>
2024-10-15 22:42:57 +0530

    initiate the RotationCarouselCustomLayout initial UI

---
+++ app/src/main/java/com/example/composelearning/customlayout/RotatedCarouseCustomLayout.kt
@@ -0,0 +1,23 @@
+package com.example.composelearning.customlayout
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
+
+@Composable
+fun RotationCarouselCustomLayout(
+    numberOfItems: Int,
+    itemFraction: Float = .25f,
+    modifier: Modifier = Modifier,
+    content: @Composable (index: Int) -> Unit
+) {
+
+    Layout(modifier = modifier, content = { /*TODO*/ }) { measurables, constraints ->
+        //TODO
+
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            //TODO
+        }
+    }
+
+}
