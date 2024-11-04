9d5e9afd76d3f0226aa635eb53703a2d9ef380ee
sparrow007 <ankit.steven007@gmail.com>
2024-11-04 09:56:46 +0530

    added the itemProvider

--- app/src/main/java/com/example/composelearning/customlayout/ParallaxCarouselLayout.kt
+++ app/src/main/java/com/example/composelearning/customlayout/ParallaxCarouselLayout.kt
@@ -1,16 +1,82 @@
 package com.example.composelearning.customlayout
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.layout.LazyLayoutItemProvider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
+
+@Composable
+fun LazyLayoutInfiniteScroll(
+
+) {
+
+}
+
+@ExperimentalFoundationApi
+class ItemProvider(
+    private val itemState: State<List<LazyLayoutItemContent>>
+) : LazyLayoutItemProvider {
+
+    override val itemCount: Int
+        get() = itemState.value.size
+
+    @Composable
+    override fun Item(index: Int, key: Any) {
+        val item = itemState.value.getOrNull(index)
+        item?.content?.invoke(item.item)
+    }
+
+    fun getItemIndexInRanges(viewBoundaries: ViewBoundaries): List<Int> {
+        val result = mutableListOf<Int>()
+        itemState.value.forEachIndexed { index, item ->
+            if (item.item.x in viewBoundaries.fromX..viewBoundaries.toX &&
+                item.item.y in viewBoundaries.fromY..viewBoundaries.toY
+            ) {
+                result.add(index)
+            }
+        }
+        return result
+    }
+
+    fun getItem(index: Int): ListItem? {
+        return itemState.value.getOrNull(index)?.item
+    }
+
+}
+
+
+data class ListItem(
+    val x: Int,
+    val y: Int
+)
+
+data class ViewBoundaries(
+    val fromX: Int,
+    val toX: Int,
+    val fromY: Int,
+    val toY: Int
+)
+
+typealias ComposeItemContent = @Composable (ListItem) -> Unit
+
+class LazyLayoutItemContent(
+    val item: ListItem,
+    val content: ComposeItemContent
+)
+
+
+
+
 
 @Composable
 fun SnapCarouselLayout(
